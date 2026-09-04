d134f2d059dce9ec09fba461b3efe92bec71c620
Rahul Varat <rahulvarat5@gmail.com>
2026-09-04 15:42:06 +0530

    Implement containsNearbyDuplicate function

---
+++ leetcode-219.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>f;
+        for(int i=0;i<nums.size();i++){
+            if(f.count(nums[i])){
+            if(abs(f[nums[i]]-i)<=k){
+              
+                    return true;
+            }
+            }
+            f[nums[i]]=i;
+
+        }
+        return false;
+    }
+};
